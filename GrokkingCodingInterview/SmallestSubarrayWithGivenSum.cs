155e5bec44af40eb8ac0bac18e658570d70974b1
moshi <moshiur@cefalo>
2020-06-22 02:58:19 +0600

    Smallest Subarray with a given sum

---
+++ GrokkingCodingInterview/SmallestSubarrayWithGivenSum.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SlidingWindow
+{
+    public class SmallestSubarrayWithGivenSum
+    {
+        public int GetSmallestSubarrayElementCount(int[] array, int sum)
+        {
+            int minLength = int.MaxValue;
+
+            int windowStart = 0;
+            int windowEnd = 0;
+            float windowSum = 0;
+            while (windowEnd < array.Length)
+            {
+                windowSum += array[windowEnd];
+
+                while (windowSum >= sum)
+                {
+                    windowSum -= array[windowStart];
+                    int windowSize = windowEnd - windowStart + 1;
+                    minLength = Math.Min(windowSize, minLength);
+                    windowStart++;
+                }
+
+                windowEnd++;
+            }
+
+            return minLength == int.MaxValue ? 0 : minLength;
+        }
+    }
+}
---
+++ SlidingWindowTestProject/SmallestSubarrayWithGivenSumTests.cs
@@ -0,0 +1,59 @@
+﻿using SlidingWindow;
+using Xunit;
+
+namespace SlidingWindowTestProject
+{
+    public class SmallestSubarrayWithGivenSumTests
+    {
+        private SmallestSubarrayWithGivenSum smallestSubarrayWithGivenSum;
+        public SmallestSubarrayWithGivenSumTests()
+        {
+            smallestSubarrayWithGivenSum = new SmallestSubarrayWithGivenSum();
+        }
+
+        [Fact]
+        public void GetSmallestSubarrayElementCount_Positive1()
+        {
+            var array = new int[] { 2, 1, 5, 2, 3, 2 };
+            int sum = 7;
+
+            var actual = smallestSubarrayWithGivenSum.GetSmallestSubarrayElementCount(array, sum);
+            var expected = 2;
+            Assert.Equal(expected,actual);
+        }
+
+        [Fact]
+        public void GetSmallestSubarrayElementCount_Positive2()
+        {
+            var array = new int[] { 2, 1, 5, 2, 8 };
+            int sum = 7;
+
+            var actual = smallestSubarrayWithGivenSum.GetSmallestSubarrayElementCount(array, sum);
+            var expected = 1;
+            Assert.Equal(expected, actual);
+        }
+
+
+        [Fact]
+        public void GetSmallestSubarrayElementCount_Positive3()
+        {
+            var array = new int[] { 3, 4, 1, 1, 6 };
+            int sum = 8;
+
+            var actual = smallestSubarrayWithGivenSum.GetSmallestSubarrayElementCount(array, sum);
+            var expected = 3;
+            Assert.Equal(expected, actual);
+        }
+
+        [Fact]
+        public void GetSmallestSubarrayElementCount_Positive4()
+        {
+            var array = new int[] { 3, 4, 1, 1, 6 };
+            int sum = 80;
+
+            var actual = smallestSubarrayWithGivenSum.GetSmallestSubarrayElementCount(array, sum);
+            var expected = 0;
+            Assert.Equal(expected, actual);
+        }
+    }
+}
