e4fafa942c9cf0cff363f700378a40e3442600bb
moshi <moshiur@cefalo>
2020-06-22 03:11:21 +0600

    complexity analysis added

--- GrokkingCodingInterview/SmallestSubarrayWithGivenSum.cs
+++ GrokkingCodingInterview/SmallestSubarrayWithGivenSum.cs
@@ -1,9 +1,16 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Text;
 
 namespace SlidingWindow
 {
+
+    ///<summary>
+    ///Time Complexity #
+    ///The time complexity of the above algorithm will be O(N). 
+    ///The outer for loop runs for all elements and the inner while loop processes each element only once, 
+    ///therefore the time complexity of the algorithm will be O(N+N) which is asymptotically equivalent to O(N).
+    ///Space Complexity #
+    ///The algorithm runs in constant space O(1).
+    /// </summary>
     public class SmallestSubarrayWithGivenSum
     {
         public int GetSmallestSubarrayElementCount(int[] array, int sum)
