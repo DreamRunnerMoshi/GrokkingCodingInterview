616842779c5ac985a1e50241a82b7892cea45115
moshi <moshiur@cefalo>
2020-06-25 00:22:41 +0600

    Fruits into basket

---
+++ GrokkingCodingInterview/FruitsIntoBaskets.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SlidingWindow
+{
+    public class FruitsIntoBaskets
+    {
+        public int GetMaxCount(char[] array)
+        {
+            int k = 2;
+            int windowStart = 0;
+            int windowEnd = 0;
+            var dictionary = new Dictionary<char, int>();
+            int maxSize = int.MinValue;
+
+            while (windowEnd < array.Length)
+            {
+
+                char c = array[windowEnd];
+                if (!dictionary.ContainsKey(c))
+                {
+                    dictionary[c] = 0;
+                }
+                dictionary[c]++;
+
+                while (dictionary.Keys.Count > k)
+                {
+                    var startChar = array[windowStart++];
+                    dictionary[startChar]--;
+                    if (dictionary[startChar] == 0)
+                    {
+                        dictionary.Remove(startChar);
+                    }
+                }
+                maxSize = Math.Max(windowEnd - windowStart + 1, maxSize);
+                windowEnd++;
+            }
+
+            return maxSize;
+        }
+    }
+}
---
+++ SlidingWindowTestProject/FruitsIntoBasketsTests.cs
@@ -0,0 +1,36 @@
+﻿using SlidingWindow;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+
+namespace SlidingWindowTestProject
+{
+    public class FruitsIntoBasketsTests
+    {
+        readonly FruitsIntoBaskets fruitsIntoBaskets;
+        public FruitsIntoBasketsTests()
+        {
+            fruitsIntoBaskets = new FruitsIntoBaskets();
+        }
+
+
+        [Fact]
+        public void FruitsIntoBaskets_Positive()
+        {
+            var array = new char[] { 'A', 'B', 'C', 'A', 'C' };
+            var actual = fruitsIntoBaskets.GetMaxCount(array);
+            var expected = 3;
+            Assert.Equal(actual, expected);
+        }
+
+        [Fact]
+        public void FruitsIntoBaskets_Positive1()
+        {
+            var array = new char[] { 'A', 'B', 'C', 'B', 'B', 'C' };
+            var actual = fruitsIntoBaskets.GetMaxCount(array);
+            var expected = 5;
+            Assert.Equal(actual, expected);
+        }
+    }
+}
