1ee0f52149c9e4c4e22e02e8813b09240709e891
moshi <moshiur@cefalo>
2020-06-25 00:11:55 +0600

    Longest Substring with K Distinct Characters

---
+++ GrokkingCodingInterview/LongestSubstringWithKDistinctChars.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Runtime.InteropServices.ComTypes;
+
+namespace SlidingWindow
+{
+    public class LongestSubstringWithKDistinctChars
+    {
+        public int GetLongestSubstringLength(string input, int k)
+        {
+            int windowStart = 0;
+            int windowEnd = 0;
+            var dictionary = new Dictionary<char, int>();
+            int maxSize = int.MinValue;
+            while (windowEnd < input.Length)
+            {
+
+                char c = input[windowEnd];
+                if (!dictionary.ContainsKey(c))
+                {
+                    dictionary[c] = 0;
+                }
+                dictionary[c]++;
+
+                while (dictionary.Keys.Count > k)
+                {
+                    var startChar = input[windowStart++];
+                    dictionary[startChar]--;
+                    if (dictionary[startChar] == 0)
+                    {
+                        dictionary.Remove(startChar);
+                    }
+                }
+                maxSize = Math.Max(windowEnd - windowStart + 1, maxSize);
+                windowEnd++;
+            }
+            return maxSize;
+        }
+    }
+}
---
+++ SlidingWindowTestProject/LongestSubstringWithKDistinctCharsTests.cs
@@ -0,0 +1,49 @@
+﻿using SlidingWindow;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+
+namespace SlidingWindowTestProject
+{
+    public class LongestSubstringWithKDistinctCharsTests
+    {
+        readonly LongestSubstringWithKDistinctChars longestSubstringWithKDistinctChars;
+        public LongestSubstringWithKDistinctCharsTests()
+        {
+            longestSubstringWithKDistinctChars = new LongestSubstringWithKDistinctChars();
+        }
+
+        [Fact]
+        public void LongestSubstringWithKDistinctChars_Positive()
+        {
+            String input = "araaci";
+            int k = 2;
+
+            var actual = longestSubstringWithKDistinctChars.GetLongestSubstringLength(input, k);
+            var expected = 4;
+            Assert.Equal(expected, actual);
+        }
+
+        [Fact]
+        public void LongestSubstringWithKDistinctChars_PositiveK1()
+        {
+            String input = "araaci";
+            int k = 1;
+
+            var actual = longestSubstringWithKDistinctChars.GetLongestSubstringLength(input, k);
+            var expected = 2;
+            Assert.Equal(expected, actual);
+        }
+        [Fact]
+        public void LongestSubstringWithKDistinctChars_PositiveK0()
+        {
+            String input = "araaci";
+            int k = 0;
+
+            var actual = longestSubstringWithKDistinctChars.GetLongestSubstringLength(input, k);
+            var expected = 0;
+            Assert.Equal(expected, actual);
+        }
+    }
+}
