fcf2139695ed5c039d8fc9877e4f9a3e7d7d833f
moshi <moshiur@cefalo>
2020-06-29 02:21:35 +0600

    reorganized

---
+++ SlidingWindow/LngstSubWithSameLetterAfterRplcmnt.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace SlidingWindow
+{
+    public class LngstSubWithSameLetterAfterRplcmnt
+    {
+        public int GetLongestSubstringLength(string input, int k)
+        {
+            int windowStart = 0;
+            int windowEnd = 0;
+            int maxLength = int.MinValue;
+
+            var letterFrequency = new Dictionary<char, int>();
+            int maxRepetingCount = 0;
+
+            while (windowEnd < input.Length)
+            {
+                var endChar = input[windowEnd];
+
+                if (!letterFrequency.ContainsKey(endChar))
+                {
+                    letterFrequency[endChar] = 0;
+                }
+                letterFrequency[endChar]++;
+
+                maxRepetingCount = Math.Max(maxRepetingCount, letterFrequency[endChar]);
+
+                if (windowEnd - windowStart - maxRepetingCount + 1 > k)
+                {
+                    var startChar = input[windowStart];
+                    letterFrequency[startChar]--;
+                    windowStart++;
+                }
+
+                maxLength = Math.Max(maxLength, windowEnd - windowStart + 1);
+                windowEnd++;
+            }
+
+            return maxLength;
+        }
+    }
+}
---
+++ SlidingWindowTestProject/LngstSubWithSameLetterAfterRplcmntTests.cs
@@ -0,0 +1,37 @@
+﻿using SlidingWindow;
+using Xunit;
+
+namespace SlidingWindowTestProject
+{
+    public class LngstSubWithSameLetterAfterRplcmntTests
+    {
+        private LngstSubWithSameLetterAfterRplcmnt lng;
+
+        public LngstSubWithSameLetterAfterRplcmntTests()
+        {
+            lng = new LngstSubWithSameLetterAfterRplcmnt();
+        }
+
+        [Fact]
+        public void LngstSubWithSameLetterAfterRplcmnt_Positive()
+        {
+            var input = "aabccbb";
+            int k = 2;
+            var actual = lng.GetLongestSubstringLength(input,k);
+            var expected = 5;
+
+            Assert.Equal(expected,actual);
+        }
+
+        [Fact]
+        public void LngstSubWithSameLetterAfterRplcmnt_Positive1()
+        {
+            var input = "aabbab";
+            int k = 2;
+            var actual = lng.GetLongestSubstringLength(input, k);
+            var expected = 5;
+
+            Assert.Equal(expected, actual);
+        }
+    }
+}
