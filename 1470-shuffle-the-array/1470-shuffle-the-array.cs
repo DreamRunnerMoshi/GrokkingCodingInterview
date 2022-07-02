6e44d1026aec153a98cab455711dfd70462d5b2a
Moshiur Rahman Moshi <moshi.iit@gmail.com>
2022-07-03 02:06:56 +0700

    Time: 177 ms (66.67%), Space: 43.3 MB (55.15%) - LeetHub

--- 1470-shuffle-the-array/1470-shuffle-the-array.cs
+++ 1470-shuffle-the-array/1470-shuffle-the-array.cs
@@ -1,13 +1,11 @@
 public class Solution {
     public int[] Shuffle(int[] nums, int n) {
         
-        var mid = n;
-        
         var newArray = new int [2*n];
         
-        for(int i=0; i<mid; i++) {
+        for(int i=0; i<n; i++) {
             newArray[i*2] = nums[i];
-            newArray[i*2 + 1] = nums[mid+i];
+            newArray[i*2 + 1] = nums[n+i];
         }
         
         return newArray;
